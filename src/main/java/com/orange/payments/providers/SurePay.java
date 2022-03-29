e2dca6e1e1faf63e7a69291b4e40687e2ee11772
nnyanyirai <49812319+nnyanyirai@users.noreply.github.com>
2022-03-29 11:59:10 +0200

    Added Provider surePay
    Added Provider surePay

---
+++ src/main/java/com/orange/payments/providers/SurePay.java
@@ -0,0 +1,56 @@
+package com.orange.payments.providers;
+
+import com.orange.payments.PaymentProvider;
+import com.orange.payments.TransactionResult;
+import com.orange.payments.TransactionStatus;
+import com.orange.transactions.Transaction;
+
+public class SurePay implements PaymentProvider {
+
+  @Override
+  public String getName() {
+
+    return this.getClass().getSimpleName();
+  }
+
+  @Override
+  public boolean canProcessTransaction(Transaction transaction) {
+    //TODO clarify if its all transactions from India or only HoutPay transactions from India that are not supported
+    return !transaction.getCountry().getCountryCode().equals(india().getCountryCode());
+  }
+
+  @Override
+  public double calculateTransactionFee(Transaction transaction) throws UnsupportedTransactionException {
+    if(!canProcessTransaction(transaction)){
+      throw new UnsupportedTransactionException("Card not supported for this " + this.getClass().getName());
+    }
+    if(transaction.getCardType().equals(CardType.HoutPay)){
+      return transaction.getTransactionAmount() * SUREPAY_FEE_FOR_HOUTPAY;
+    }
+    if(transaction.getTransactionAmount()<=500){
+      return transaction.getTransactionAmount() * SUREPAY_FEE_FOR_AMOUNT_LESS_THAN_500;
+    }
+    return transaction.getTransactionAmount() * SUREPAY_FEE_FOR_AMOUNT_GREATER_THAN_500;
+  }
+
+  @Override
+  public TransactionResult processTransaction(Transaction transaction) throws UnsupportedTransactionException{
+    if (canProcessTransaction(transaction)) {
+      double totalFee = transaction.getTransactionAmount() * ORANGE_FEE;
+      double receiverWillGet = transaction.getTransactionAmount() - totalFee;
+      double forwardToProvider = 0;
+      forwardToProvider = receiverWillGet / (1 - calculateTransactionFee(transaction));
+      return new TransactionResult(
+          getName(),
+          TransactionStatus.Success,
+          calculateTransactionFee(transaction),
+          "Transaction successful.",
+          transaction,
+          totalFee,
+          receiverWillGet,
+          forwardToProvider);
+    }
+    return new TransactionResult(getName(), TransactionStatus.Failed, 0, "Card not supported.",
+        transaction, 0.0, 0.0, 0.0);
+  }
+}
